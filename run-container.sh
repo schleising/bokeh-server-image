55ce9523d22a1b3717ef2988db36a29581030a5a
Stephen Schleising <stephen@schleising.net>
2020-08-31 10:11:25 +0100

    Enable quick restart of all containers

--- run-container.sh
+++ run-container.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
+docker rm --force bokeh-server
 docker run -p 5006:5006 --name bokeh-server --network schleising-net -v /Users/steve/Documents/Coding/Python/website/mainsite/charts/BokehPlots/:/code/ bokeh-server-image
